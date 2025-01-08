717ddfbf14317c0ebaf213108e5bf74f1aa6bda0
anasoid <oufdou.anas@gmail.com>
2025-01-08 12:18:44 +0100

    remove notnull check

--- src/main/java/org/anasoid/instancio/petclinic/jpa/example/core/properties/SampleDataProperties.java
+++ src/main/java/org/anasoid/instancio/petclinic/jpa/example/core/properties/SampleDataProperties.java
@@ -1,6 +1,6 @@
 package org.anasoid.instancio.petclinic.jpa.example.core.properties;
 
-import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,26 +14,19 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "instancio.sample.data.config")
 public class SampleDataProperties {
 
-    @NotNull
+
     private Integer dataSize;
-    @NotNull
     private String csvPath;
-    @NotNull
     Map<String, GeneratorProperties> entities;
 
     @Getter
     @Setter
     public static class GeneratorProperties {
-        @NotNull
-        private Integer minElement;
-        @NotNull
-        private Integer maxElement;
-        @NotNull
-        private Integer percentElement;
 
+        private Integer minElement;
+        private Integer maxElement;
+        private Integer percentElement;
         private Boolean forceGenerateElement;
 
-        public GeneratorProperties() {
-        }
     }
 }
